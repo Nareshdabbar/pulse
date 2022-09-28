2a7e438fc299feb30a286895a86f99355a8a8059
naresh <nareshdabbar@gmail.com>
2022-09-28 18:25:54 +0530

    hhh

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
   return (
     <div className="App">
 
-      <h1>anil is clond</h1>
-
-hello word
-hello anil how are you
-<h2>jshvdakjfgkjssaghdfzvj</h2>
+     
     <SignUp></SignUp>
     <Login></Login>
     </div>
