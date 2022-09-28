e3ff1fd9abb5e39082ba21535ba430f7e87688f1
anil <anilkumarg9700@gmail.com>
2022-09-28 16:52:47 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SignUp from './component/SignUp';
+import SignUp from './Component/SignUp';
 import Login from './Component/Login';
 
 function App() {
